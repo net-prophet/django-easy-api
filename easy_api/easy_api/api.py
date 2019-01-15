6b54bc2154d36ebef8dd3d681064e70118468cf7
Robert Wells <wellsroberte@gmail.com>
2019-01-15 13:57:17 -0800

    private user API registered

--- easy_api/easy_api/api.py
+++ easy_api/easy_api/api.py
@@ -1,4 +1,5 @@
-from EasyAPI.models import EasyAPI
+from django.contrib.auth.models import User
+from EasyAPI.models import EasyAPI, ModelAPI
 from rest_framework import permissions
 
 publicapi = EasyAPI('Public API',
@@ -13,3 +14,11 @@ complexapi = EasyAPI('Complex API',
                      permissions.AllowAny,
                      'API for testing a complicated permissions structure'
                      )
+
+
+class PrivateUserAPI(ModelAPI):
+    api_fields = ('username', 'email', 'first_name', 'last_name',
+                  'is_superuser', 'last_login', 'date_joined')
+
+
+privateapi.register(User, PrivateUserAPI)
