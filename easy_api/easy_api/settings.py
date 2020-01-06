afdbb654c4c4142e5e914db3b0e73b260fe0cb40
Leeward Bound <l@lwb.co>
2020-01-06 10:58:35 +0000

    moving docs

--- easy_api/easy_api/settings.py
+++ easy_api/easy_api/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'EasyAPI',
     'rest_framework',
     'django_filters',
+    'corsheaders',
 ]
 
 
@@ -45,6 +46,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:6006',
+    'http://localhost:6006',
+)
+
+CORS_ORIGIN_ALLOW_ALL=True
